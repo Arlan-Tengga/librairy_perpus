8349941f3d2feb7b44bfd3d2a120b72585505047
Arlan Tengga <arlantengga8@gmail.com>
2025-01-01 04:01:14 -0800

    Update __init__.py

--- CRI/__init__.py
+++ CRI/__init__.py
@@ -1,4 +1,3 @@
 from .DATABASE  import init_console
-from .View import read_console
+from .View import read_console,update_console,delete_console
 from .create import create_console
-from .update import update_console
