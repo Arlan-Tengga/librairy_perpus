cbec6d28cbba1b93450dfe1a3b8e8393115629cf
Arlan Tengga <arlantengga8@gmail.com>
2025-01-01 04:03:35 -0800

    Update operasi.py

--- CRI/operasi.py
+++ CRI/operasi.py
@@ -2,6 +2,28 @@ from . import DATABASE
 from .Util import random_string
 import time
 
+def update(no_buku,pk,data_add,tahun,judul,penulis):
+    data = DATABASE.TEMPLATE.copy()
+
+    data["pk"] = pk
+    data["date_add"] = data_add
+    data["penulis"] = penulis + DATABASE.TEMPLATE["penulis"][len(penulis):]
+    data["judul"] = judul + DATABASE.TEMPLATE["judul"][len(judul):]
+    data["penulis"] = judul
+    data["Tahun"] = tahun
+
+    data_str = f'{data["pk"]},{data["date_add"]},{data["penulis"]},{data["judul"]},{data["Tahun"]}\n'
+    panjang_data = len(data_str)
+
+    try:
+        with(open(DATABASE.DB_NAME,'r+',encoding="utf-8"))as file :
+            file.seek(panjang_data*(no_buku-1))
+            file.write(data_str)
+    except:
+        print("erorr bosss")
+
+
+
 def create(tahun,judul,penulis):
     data = DATABASE.TEMPLATE.copy()
 
@@ -55,13 +77,22 @@ def create_first_data():
     except:
         print("Gagal boss")
 
-def read():
+def read(**kwargs):
     try:
         with open(DATABASE.DB_NAME, 'r') as file:
             content = file.readlines()
-            return content
+            jumlah_buku = len(content)
+            if "index" in kwargs:
+                index_buku = kwargs["index"]-1
+                if index_buku < 0 or index_buku > jumlah_buku:
+                    return False
+                else :
+                    return content[index_buku]
+            else :
+                return content
     except:
         print("Membaca database error")
         return False
+
 
 
