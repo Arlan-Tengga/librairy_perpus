5b0019c3bf607892fed01c504271d439600aa37b
Arlan Tengga <arlantengga8@gmail.com>
2025-01-01 04:05:29 -0800

    Update View.py

--- CRI/View.py
+++ CRI/View.py
@@ -1,5 +1,93 @@
 from .import operasi
 
+def delete_console():
+    read_console()
+    while(True):
+        print("Silahkan pilih nomor buku yang akan di delete ")
+        no_buku = int(input("Masukkan nomor buku : "))
+        data_buku = operasi.read(index=no_buku)
+
+        if data_buku:
+            data_break = data_buku.split(",")
+            pk = data_break[0]
+            date_add = data_break[1]
+            penulis = data_break[2]
+            judul = data_break[3]
+            tahun = data_break[4]
+
+            print("\n"+"="*100)
+            print("Data yang ingin anda Hapus")
+            print(f"1. Judul\t: {judul:.40}")
+            print(f"2. Penulis\t: {penulis:.40}")
+            print(f"3. Tahun\t: {tahun:4}")
+            is_done = input("Apakah anda yakin (y/n)? ")
+            if is_done == "y" or is_done == "Y":
+                operasi.delete(no_buku)
+                break
+        else:
+            print("nomor tidak valid, silahkan masukan lagi")
+
+    print("Data berhasil di hapus")
+        
+
+    while(True):
+        print("\n"+"="*100)
+        print("Silahkan pilih data yakin ingin mngubahnya? ")
+        print(f"1. judul\t: {judul:.40}")
+        print(f"2. Penulis\t: {penulis:.40}")
+        print(f"3. Tahun\t: {tahun:4}")
+        is_done = input("Apakah Sudah selesai (y/n): ")
+        if is_done == "y" or is_done == "Y" :
+            break
+
+def update_console():
+    read_console()
+    while(True):
+        print("Silahkan pilih nomor buku yang akan di update")
+        no_buku = int(input("Masukkan nomor buku : "))
+        data_buku = operasi.read(index=no_buku)
+
+        if data_buku:
+            break
+        else:
+            print("nomor tidak valid,silahkan masukkan lagi")
+    
+    data_break = data_buku.split(',')
+    pk = data_break[0]
+    data_add = data_break[1]
+    penulis = data_break[2]
+    judul = data_break[3]
+    tahun = data_break[4][:-1]
+
+    while(True):
+        print("\n"+"="*100)
+        print("Silahkan pilih data yang anda ingin anda ubah")
+        print(f"1. judul\t: {judul:.40}")
+        print(f"2. Penulis\t: {penulis:.40}")
+        print(f"3. Tahun\t: {tahun:4}")
+
+        user_option = input("Pilih data [1,2,3]: ")
+        print("\n"+"="*100)
+        match user_option:
+            case "1": judul = input("judul\t: ")
+            case "2": penulis = input("penulis\t: ")
+            case "3":
+                while(True):
+                    try :
+                        tahun = int(input("tahun\t:"))
+                        if len(str(tahun)) == 4:
+                            break
+                        else:
+                            print("masukkan angka dan yyyy")
+                    except :
+                        print("masukkan format angka dan yyyy")
+        
+        is_done = input("Apakah Sudah selesai (y/n): ")
+        if is_done == "y" or is_done == "Y" :
+            break
+    operasi.update(no_buku,pk,data_add,tahun,judul,penulis)
+
+
 def read_console():
     data_file = operasi.read()
     
